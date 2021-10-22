2b7fbf48624f243387a0d1001fda3c0fafa320da
Tuong Nguyen <nctuong99@gmail.com>
2021-10-22 10:36:55 +0700

    fix User.phone cannot be null

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const express = require('express');
 const cors = require('cors');
 const authRouter = require('./routes/auth.route');
 const postRouter = require('./routes/post.route');
-const uiRouter = require('./routes/ui.route')
+const uiRouter = require('./routes/ui.route');
 const buildingRouter = require('./routes/building.router');
-const roomRouter = require('./routes/room.router')
+const roomRouter = require('./routes/room.router');
 const suggestions = require('./routes/suggestions.router');
 
 const port = process.env.PORT || 7777;
@@ -17,12 +17,18 @@ const chalk = require('chalk');
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
+
+app.use((req, res, next) => {
+  console.log(req.method + ': ' + req.url);
+  next();
+});
+
 app.use('/auth', authRouter);
 app.use('/posts', postRouter);
 app.use('/ui', uiRouter);
 app.use('/building', buildingRouter);
 app.use('/room', roomRouter);
-app.use('/suggestions', suggestions)
+app.use('/suggestions', suggestions);
 
 app.listen(port, async () => {
   console.log(chalk.green(`server running at port ${port}`));
