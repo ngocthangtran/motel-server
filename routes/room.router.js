4abbdfa42a53c85c8748c38b479571c94c0f137b
Trần Thăng <ngocthangtrantb@gmail.com>
2021-12-23 02:48:34 +0700

    remove form-data on room

--- routes/room.router.js
+++ routes/room.router.js
@@ -1,27 +1,16 @@
 const express = require('express');
 const auth = require('../middleware/auth');
-const multer = require('multer');
 const { createRoom, deleteRoom, getAllRoom } = require('../controllers/room.controller');
 const imageResize = require('../middleware/imageResize');
 const { validateBuildId } = require('../middleware/validate/building');
 const { validateRoomTypeId } = require('../middleware/validate/roomType');
-const { validateUtilityIds } = require('../middleware/validate/utility');
 const { validateRoomId } = require('../middleware/validate/room');
 const router = express.Router();
-
-const MAX_IMAGE_COUNT = 6;
-
-const upload = multer({
-    dest: 'uploads/',
-    limits: { fieldSize: 25 * 1024 * 1024 },
-});
 
 router.post(
     '/create',
     [
         auth,
-        upload.array('images', MAX_IMAGE_COUNT),
-        imageResize,
         validateBuildId,
         validateRoomTypeId
     ],
