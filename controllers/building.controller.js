6b5f3d70418d9e0f0b2c59bbf6dee606b71c8e3a
Trần Thăng <ngocthangtrantb@gmail.com>
2022-01-05 21:43:52 +0700

    update

--- controllers/building.controller.js
+++ controllers/building.controller.js
@@ -77,7 +77,7 @@ const getBuilding = async (req, res) => {
 
 const deleteBulding = async (req, res) => {
     const { userId } = req.user;
-    const { buildingId } = req.query;
+    const { buildingId } = req.params;
 
     try {
         const result = await Building.destroy({
