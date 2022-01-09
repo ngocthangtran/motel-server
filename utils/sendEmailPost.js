f4bcdbf5790094769402507c821771f77ba49f87
Trần Thăng <ngocthangtrantb@gmail.com>
2022-01-09 11:15:55 +0700

    fix send email

--- utils/sendEmailPost.js
+++ utils/sendEmailPost.js
@@ -15,15 +15,23 @@ const sendMail = (req, res) => {
     })
 
     //send user
-    mailOtionUser = {
+    const mailOtionUser = {
         from: account.user,
         to: req.user.email,
         subject: "Chờ xác thực bài đăng TroVN",
-        html: `<h3>Cám ơn bạn ${res.user.name} đã tin tưởng và sử dụng phần mềm của chúng tôi!</h3>
+        html: `<h3>Cám ơn bạn ${req.user.name} đã tin tưởng và sử dụng phần mềm của chúng tôi!</h3>
     <p>Bài đăng của bạn đang được các admin kiểm duyệt sẽ và sẽ được đăng trong thời gian sớm nhất.</br>
     Trong thời gian chờ đợi bạn có thể xem qua các chức năng khác của ứng dụng như quản lý nhà trọ cho bạn.</br>
     Chức năng đang trong quá trình phát triển và được sử dụng hoàn toàn miễn phí. Hãy góp ý giúp chúng tôi thông quan email này</p>`
     }
+
+    transporter.sendMail(mailOtionUser, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    })
 
     //send add admin
 }
